14f6105d1b30255123a4e7cf4695911ff2e99664
aidanshilling <aidanshilling91@gmail.com>
2019-11-02 01:12:29 -0400

    Added friends functionality

--- app.js
+++ app.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.render('home');
+var friends = [ 'Tony', 'Niraj', 'Andrew', 'Ryleigh', 'Stuart', 'Tammy' ];
+
+app.get('/', function(req, res) {
+	res.render('home');
 });
 
-app.listen(3000, () =>{
-    console.log('Listening on port 3000')
+app.post('/addfriend', function(req, res) {
+	var newFriend = req.body.newfriend;
+	friends.push(newFriend);
+	res.redirect('/friends');
+});
+
+app.get('/friends', function(req, res) {
+	res.render('friends', { friends: friends });
+});
+
+app.listen(3000, () => {
+	console.log('Listening on port 3000');
 });
