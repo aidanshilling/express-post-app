002d1b456a0b4b09bf1b44d6c9d875128387b06c
aidanshilling <aidanshilling91@gmail.com>
2019-11-02 00:54:25 -0400

    Add app.js and home.ejs (basic skeleton)

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res){
+    res.render('home');
+});
+
+app.listen(3000, () =>{
+    console.log('Listening on port 3000')
+});
